9fac453b8f9bcb51797d68c30ee3351f26a8b716
MarioMingoia <MM3417@live.mdx.ac.uk>
2023-10-13 20:40:48 +0100

    exit

--- Assets/Scripts/mainMenu.cs
+++ Assets/Scripts/mainMenu.cs
@@ -11,6 +11,9 @@ public class mainMenu : MonoBehaviour
     public GameObject gameNameCube;
 
     bool stopRotate = false;
+
+    public bool nextLevelBool = false;
+    public bool exitGameBool = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,7 +25,7 @@ public class mainMenu : MonoBehaviour
     {
         if (!stopRotate)
         {
-            gameNameCube.transform.Rotate(1, 0, 1);
+            gameNameCube.transform.Rotate(.5f, 0, .5f);
         }
     }
 
@@ -30,6 +33,16 @@ public class mainMenu : MonoBehaviour
     {
         panel.SetActive(true);
         StartCoroutine(fadeIn());
+        nextLevelBool = true;
+        exitGameBool = false;
+    }
+
+    public void exitGame()
+    {
+        panel.SetActive(true);
+        StartCoroutine(fadeIn());
+        nextLevelBool = false;
+        exitGameBool = true;
     }
     IEnumerator fadeIn()
     {
@@ -37,11 +50,19 @@ public class mainMenu : MonoBehaviour
         print("fading in");
         stopRotate = true;
         panel.GetComponent<CanvasGroup>().alpha += Time.deltaTime;
-        gameNameCube.transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, new Vector3(0, 0, 0), 0.00001f * Time.fixedDeltaTime);
+        gameNameCube.transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, new Vector3(0, 0, 0), 0.01f * Time.fixedDeltaTime);
         if (panel.GetComponent<CanvasGroup>().alpha >= 1)
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-
+            if (nextLevelBool && !exitGameBool)
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            else if (!nextLevelBool && exitGameBool)
+            {
+            #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+            #endif
+                Application.Quit();
+            }
+        
             yield break;
         }
         yield return fadeIn();
